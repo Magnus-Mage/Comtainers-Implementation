df112ae8c4a060e7e5f8ffdf9e15283b50f1076e
Magnus-Mage <romescha123@gmail.com>
2025-07-08 07:33:43 +0000

    Add: Capacity related member functions

--- template_vector/vector.h
+++ template_vector/vector.h
@@ -14,7 +14,34 @@ namespace test
 		std::size_t m_size     { 0 };
 		std::size_t m_capacity { 0 };
 
-	
+		/**
+		 * @brief Check if the container is full or not
+		 * @return True if the container is full
+		 */
+	       	[[nodiscard]] bool full() { return size() == capacity(); }
+
+		/**
+		 * @brief Grow the container twice its size.
+		 */
+		void grow() { reserve(capacity() ? capacity() * 2 : 16); }
+
+	public:
+		// Capacity related member functions
+		/**
+		 * @brief Returns the number of elements in the vector.
+		 */
+		[[nodiscard]] std::size_t size() const { return m_size; }
+
+		/**
+		 * @brief Returns the total number of elements a vector can hold
+		 */
+		[[nodiscard]] std::size_t capacity() const { return m_capacity; }
+		
+		/**
+		 * @brief Checks if the vector is empty or not.
+		 * @return Returns True if the vector is empty
+		 */
+		[[nodiscrard]] bool empty() const { return size() == 0; } 
 	};
 		
 }
