fd4a6efe7aacd88c6b9d80143c68ab9d9fbf8924
Magnus-Mage <romescha123@gmail.com>
2025-07-08 06:32:19 +0000

    Add: vector class initialisation

--- template_vector/vector.h
+++ template_vector/vector.h
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <algorithms>
 
+/** 
+ * @brief A standard container that gives access to elements in O(1) time.
+ */
 namespace test
 {
 	template<typename T>
 	class vector
 	{
-		void vector_initialised()
+	private:
+		T*          m_elements { nullprt };
+		std::size_t m_size     { 0 };
+		std::size_t m_capacity { 0 };
+
+	
 	};
 		
 }
